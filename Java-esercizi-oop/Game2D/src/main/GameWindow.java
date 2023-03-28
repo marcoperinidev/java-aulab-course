01fdea10bb48e3877491ec60dd8ff26ee52580bb
marcoperinidev <info@mpdev.me>
2023-03-29 01:16:44 +0200

    base for 2d game

---
+++ Java-esercizi-oop/Game2D/src/main/GameWindow.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow {
+    private JFrame jframe;
+
+    public GameWindow(GamePanel gamePanel) {
+
+        jframe = new JFrame("Game");
+
+        jframe.setSize(600, 400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setVisible(true);
+    }
+}
