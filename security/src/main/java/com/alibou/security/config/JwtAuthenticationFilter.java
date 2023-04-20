ed6e6b22163e4572ea27e6921b32f1e373d29df3
marcoperinidev <info@mpdev.me>
2023-04-20 22:37:17 +0200

    implemented JwtAuthenticationFilter with internal filter, servlet request response and filterchain

--- security/src/main/java/com/alibou/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/alibou/security/config/JwtAuthenticationFilter.java
@@ -4,11 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor //crea un costruttore con tutti i parametri final
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
@@ -16,6 +20,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+      final String authHeader = request.getHeader("Authorization");
+      final String jwt;
+      if (authHeader == null ||!authHeader.startsWith("Bearer ")){
+          filterChain.doFilter(request, response);//passa al next filter
+          return;
+      }
+      jwt = authHeader.substring(7);//bearer + space = 7
     }
 }
